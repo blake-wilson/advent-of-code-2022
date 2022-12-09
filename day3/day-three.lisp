dd160cb2d7f21a196379b1fa3a2da8cc1d773d08
Blake Wilson <blake@yellowpapersun.com>
2022-12-08 21:31:01 -0800

    Add part two of day 3

--- day3/day-three.lisp
+++ day3/day-three.lisp
@@ -25,15 +25,50 @@
   )
 )
 
-(defun tally-matches ()
-  (let ((matches (get-matches)))
+
+
+(defun tally-matches (vals)
    (apply '+
-        (loop for match in matches
-            collect (if (equal match (char-upcase match))
-                        (- (char-code match) 38)
-                        (- (char-code match) 96)
+        (loop for val in vals
+            collect (if (equal val (char-upcase val))
+                        (- (char-code val) 38)
+                        (- (char-code val) 96)
                     )
+        )
+    )
+)
+
+(tally-matches (get-matches))
+
+; part two
+(defun get-matching-chars (fst snd)
+  (let ((seen ()))
+  (loop for fst-char across fst
+        append (loop for snd-char across snd
+            if (and (equal fst-char snd-char) (not (member fst-char seen)))
+                collect
+		(progn
+		    (setf seen (cons fst-char seen))
+                    fst-char
+                )
         )
     )
   )
 )
+
+
+(defun concat-pair (pair)
+  (concatenate 'string (first pair) (second pair))
+)
+
+(defun get-badges ()
+  (let ((rucksacks (load-rucksacks))
+	(matches ""))
+    (loop for (fst snd thd) on rucksacks by #'cdddr
+	do (setf matches (get-matching-chars (concat-pair fst) (concat-pair snd)))
+	  append (get-matching-chars (concatenate 'string matches) (concat-pair thd))
+	)
+    )
+)
+
+(tally-matches (get-badges))

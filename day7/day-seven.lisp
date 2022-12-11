f52a88ccd95702269f8e7e3ac69eec4600e31c7e
Blake Wilson <blake@yellowpapersun.com>
2022-12-11 15:41:04 -0800

    Continue work on day 7

--- day7/day-seven.lisp
+++ day7/day-seven.lisp
@@ -1,3 +1,23 @@
+; prevent endless loops when printing dirs
+(setf *print-circle* t)
+
+(defstruct command
+  (name "" :type string)
+  (arguments '() :type list)
+)
+
+(defstruct file
+  (name "" :type string)
+  (size 0 :type integer)
+)
+
+(defstruct dir
+  (name "" :type string)
+  (dirs '() :type list)
+  (files '() :type list)
+  (parent nil :type (or dir null))
+)
+
 (defun get-input ()
   (uiop:read-file-lines "input")
 )
@@ -17,19 +37,72 @@
 (defun read-command-output (stream)
   (loop for char = (peek-char nil stream nil)
             if (and char (char/= char #\$))
-                collect (read-line stream nil) into res
+                collect (uiop:split-string (read-line stream nil) :separator " ")into res
             else
                 return (values res)
   )
 )
 
-(defun run-cmds (cmds)
-  (loop for cmd in cmds
-        (let ((command (first cmd))
-              (arg (second cmd)
-            )
-         )
-  )
-  (if (equa
-
+(defun add-file (dir file)
+  (setf (dir-files dir) (cons file (dir-files dir)))
 )
+
+(defun add-dir (dir to-add)
+  (setf (dir-dirs dir) (acons (dir-name to-add) to-add (dir-dirs dir)))
+  dir
+)
+
+(defun cd (dir dir-name)
+  (let ((found (assoc dir-name (dir-dirs dir) :test #'equal)))
+    (if (equal dir-name "..")
+        (dir-parent dir)
+        (if found
+            (cdr found)
+            (let ((new-dir (make-dir :name dir-name :parent dir)))
+                (progn
+                    (add-dir dir new-dir)
+                    new-dir
+                )
+            )
+        )
+    )
+  )
+)
+
+
+; (defun read-result (items)
+;
+; )
+
+(defparameter root-dir)
+
+(defun run-cmds (cmds)
+  (let ((dir (make-dir)))
+    (loop for cmd in cmds
+          do (let (
+                   (command (first (first cmd)))
+                   (command-args (rest (first cmd)))
+                   (results (second cmd))
+                )
+                (if (equal command "cd")
+                    (setf dir (cd dir (first command-args)))
+                    (if (equal command "ls")
+                        (loop for file-pair in results
+                              do (if (equal (first file-pair) "dir")
+                                  (add-dir dir (make-dir :parent dir :name (second file-pair)))
+                                  ; else is file
+                                  (let ((file (make-file :size (parse-integer (first file-pair)) :name (second file-pair))))
+                                     (add-file dir file)
+                                  )
+                              )
+                        )
+                    )
+                )
+                ; (arg (second cmd)
+          )
+     )
+    dir
+   )
+)
+
+

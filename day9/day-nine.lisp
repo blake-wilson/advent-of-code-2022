95de8295c0753e0c5075c21360809d55314d66a1
Blake Wilson <blake@yellowpapersun.com>
2022-12-21 19:11:47 -0800

    Do day 9 part 1

--- day9/day-nine.lisp
+++ day9/day-nine.lisp
@@ -3,18 +3,15 @@
   (y 0 :type integer)
 )
 
-(defun dir-to-vec (direction amt)
+(defun dir-to-moves (direction amt)
 	(ecase direction
-		(:up (make-pos :x 0 :y amt))
-		(:down (make-pos :x 0 :y (* -1 amt)))
-		(:left (make-pos :x ( * -1 amt) :y 0))
-		(:right (make-pos :x amt :y 0))
+		(:up (make-list amt :initial-element (make-pos :x 0 :y 1)))
+		(:down (make-list amt :initial-element (make-pos :x 0 :y -1)))
+		(:left (make-list amt :initial-element (make-pos :x -1 :y 0)))
+		(:right (make-list amt :initial-element (make-pos :x 1 :y 0)))
 	)
 )
 
-(defun unbundle-moves (move)
-	
-)
 
 (defun add-pos (fst snd)
 	(make-pos :x (+ (pos-x fst) (pos-x snd)) :y (+ (pos-y fst) (pos-y snd)))
@@ -31,7 +28,7 @@
 
 (defun read-move (line)
 	(let ((vals (uiop:split-string line :separator " ")))
-		(dir-to-vec (read-direction (char (first vals) 0)) (parse-integer (second vals)))
+		(dir-to-moves (read-direction (char (first vals) 0)) (parse-integer (second vals)))
 	)
 )
 
@@ -39,19 +36,10 @@
 	(with-open-file (stream "input")
 		(loop for line = (read-line stream nil)
 			while line
-			collect (read-move line)
+			append (read-move line)
 		)
 	)
 )
-
-; (defun make-moves (moves)
-; 	(let (
-; 			(head-pos (make-pos :x 0 :y 0))
-; 			(tail-pos (make-pos :x 0 :y 0))
-; 		)
-; 		(list head-pos tail-pos)
-; 	)
-; )
 
 (defun make-moves (moves)
 	(let (
@@ -63,27 +51,50 @@
 				(progn
 					(setf head-pos (first updated))
 					(setf tail-pos (second updated))
+                                        ; (list head-pos tail-pos)
 				)
 		)) moves)
 	)
 )
 
-; return (head-pos tail-pos) 
+(defun square (num)
+  (* num num)
+)
+
+(defun dist (fst-pos snd-pos)
+  (sqrt (+
+             (square (- (pos-x fst-pos) (pos-x snd-pos)))
+             (square (- (pos-y fst-pos) (pos-y snd-pos)))
+  ))
+)
+
+(defun is-touching (head-pos tail-pos)
+  (< (dist head-pos tail-pos) 2)
+)
+
+; return (head-pos tail-pos)
 (defun update-head-and-tail (head-pos tail-pos move)
-	(let ((x-move
-		(cond
-			((<= (abs (- (pos-x head-pos) (pos-x tail-pos))) 1) 0)
-			((> (pos-x head-pos) (pos-x tail-pos)) 1)
-		 	(t -1))
-	) (y-move
-		(cond
-			((<= (abs (- (pos-y head-pos) (pos-y tail-pos))) 1) 0)
-			((> (pos-y head-pos) (pos-y tail-pos)) 1)
-		 	(t -1))
-	))
-		(list (add-pos head-pos move) (add-pos tail-pos (make-pos :x x-move :y y-move)))
-	)
+	(let* ((head-new (add-pos head-pos move))
+        (x-move
+		(if (not (is-touching head-new tail-pos))
+                    (if (/= (pos-x head-new) (pos-x tail-pos))
+                          (if (> (pos-x head-new) (pos-x tail-pos)) 1 -1)
+                          0
+                    )
+                    0
+                ))
+	(y-move
+		(if (not (is-touching head-new tail-pos))
+                    (if (/= (pos-y head-new) (pos-y tail-pos))
+                          (if (> (pos-y head-new) (pos-y tail-pos)) 1 -1)
+                          0
+                    )
+                    0
+                )
+          ))
+		(list head-new (add-pos tail-pos (make-pos :x x-move :y y-move)))
+        )
 )
 
 ; part 1
-(length (remove-duplicates (make-moves (read-moves))))
+(length (remove-duplicates (make-moves (read-moves)) :test 'equalp))

faa74032af113eddd6a3f2c61f479877fd8904dd
Blake Wilson <blake@yellowpapersun.com>
2022-12-30 14:56:18 -0800

    Clean up part 1

--- day13/day-thirteen.lisp
+++ day13/day-thirteen.lisp
@@ -1,19 +1,8 @@
-; (ql:quickload "trivia")
-
-(defun cmp-until (fst snd)
-  (let ((comparison :cont))
-    (loop for i from 0 below (length (car fst))
-      while (eq :cont (setf comparison (cmp (nth i (car fst)) (nth i (car snd)))))
-      do (format t "comparison is ~S~%~%" comparison)
-    )
-    (not (eq nil comparison))
-  )
-)
+(ql:quickload "trivia")
 
 ; returns t if fst and snd are in the correct order, nil if they are in the
 ; incorrect order, and :cont if further comparisons are needed
 (defun cmp (fst snd)
-  (format t "comparing ~S to ~S~%" fst snd)
   (trivia::match (list fst snd)
                  ; first is empty list always matches
                  ((list (eq nil) (eq nil)) :cont)
@@ -24,19 +13,17 @@
 
                  ; neither a list
                  ((list (and (type integer) a) (and (type integer) b))
-                  (progn (format t "comparing two ints~%")
                  (if (< a b)
                     t
                     (if (= a b)
-                        (progn (format t "comparing two ints~%~%") :cont)
+                        :cont
                         nil
                     )
-                  ))
                   )
+                 )
 
                  ; second a list
                  ((list (and (type integer) a) (list* b))
-                    (progn (format t "comparing&*******~%~%~%")
                     (if (= (length b) 0)
                         nil
                         (let ((cmp (cmp a (car b))))
@@ -45,7 +32,6 @@
                               cmp
                           )
                         )
-                      )
                     )
                  )
 
@@ -60,53 +46,61 @@
                  ((list (list* a) (list* b))
                   (let ((car-cmp (cmp (car a) (car b))))
                     (if (eq car-cmp :cont)
-                        (progn (format t "comparing cdrs") (cmp (cdr a) (cdr b)))
+                        (cmp (cdr a) (cdr b))
                         car-cmp
                     )
                   ))
   )
 )
 
-(defun read-input ()
-  (with-open-file (stream "input")
-    (loop for cmp1 = (read-line stream nil)
-          while cmp1
-          collect (
-               let ((cmp (cmp-until (parse-packet cmp1)
-                    (parse-packet (read-line stream nil)))
-                    ))
-                    (read-line stream nil)
-                    (format t "comparison ~S is ~S~%~%" cmp1 cmp)
-                    cmp
-            )
+(defun cmp-until (fst snd)
+  (let ((comparison :cont))
+    (loop for i from 0 below (length (car fst))
+      while (eq :cont (setf comparison (cmp (nth i (car fst)) (nth i (car snd)))))
     )
+    (not (eq nil comparison))
   )
 )
 
-; func for part 1
-(defun get-right-order-index-sum ()
-  (let* ((cmps (read-input))
-         (idx 1)
-         (num-in-order 0)
-         (idx-sum 0))
-        (mapcar (lambda (in-order)
-                (format t "index is now ~d~%" idx)
-                (if in-order
-                  (progn
-                    (incf idx-sum idx)
-                (format t "index sum is now ~d~%" idx-sum)
-                    (incf num-in-order)
+
+(defun parse-number (str ptr)
+  (values (coerce (loop for c across (subseq str ptr)
+      while (digit-char-p c)
+          collect (progn
+                    (incf ptr 1)
+                     c
+              )
+   )'string) ptr)
+)
+
+(defun read-list (stack ptr)
+  (let ((lst nil))
+    (loop
+        for val = (nth ptr stack)
+        while (< ptr (length stack))
+        do (if (eq val #\[)
+                (multiple-value-bind (new-lst new-ptr) (read-list stack (+ ptr 1))
+                  (setf ptr new-ptr)
+                  (setf lst (append lst (list new-lst)))
+                )
+            (if (eq val #\])
+                 (progn
+                    (incf ptr 1)
+                    (return lst)
+                 )
+                 (progn
+                      (incf ptr 1)
+                      (setf lst (append lst (list val)))
                   )
-                ) (incf idx)) cmps)
-    (format t "~d were in order ~%" num-in-order)
-    idx-sum
+            )
+        )
+    )
+    (values lst ptr)
   )
 )
 
 (defun parse-packet (str)
   (let ((tokens '())
-        (items '())
-        (res '())
         (comma-delimited (uiop:split-string str :separator ",")
     ))
     (loop for i from 0 below (length comma-delimited)
@@ -133,41 +127,35 @@
   )
 )
 
-(defun parse-number (str ptr)
-  (let ((digit-chars '()))
-    (values (coerce (loop for c across (subseq str ptr)
-        while (digit-char-p c)
-            collect (progn
-                      (incf ptr 1)
-                       c
-                )
-     )'string) ptr)
+(defun read-input ()
+  (with-open-file (stream "input")
+    (loop for cmp1 = (read-line stream nil)
+          while cmp1
+          collect (
+               let ((cmp (cmp-until (parse-packet cmp1)
+                    (parse-packet (read-line stream nil)))
+                    ))
+                    (read-line stream nil)
+                    cmp
+            )
+    )
   )
 )
 
-(defun read-list (stack ptr)
-  (let ((lst nil))
-    (loop
-        for val = (nth ptr stack)
-        while (< ptr (length stack))
-        do (if (eq val #\[)
-                (multiple-value-bind (new-lst new-ptr) (read-list stack (+ ptr 1))
-                  (setf ptr new-ptr)
-                  (setf lst (append lst (list new-lst)))
-                )
-            (if (eq val #\])
-                 (progn
-                    (incf ptr 1)
-                    (return lst)
-                 )
-                 (progn
-                      (incf ptr 1)
-                      (setf lst (append lst (list val)))
+; func for part 1
+(defun get-right-order-index-sum ()
+  (let* ((cmps (read-input))
+         (idx 1)
+         (num-in-order 0)
+         (idx-sum 0))
+        (mapcar (lambda (in-order)
+                (if in-order
+                  (progn
+                    (incf idx-sum idx)
+                    (incf num-in-order)
                   )
-            )
-        )
-    )
-    (values lst ptr)
+                ) (incf idx)) cmps)
+    idx-sum
   )
 )
 
